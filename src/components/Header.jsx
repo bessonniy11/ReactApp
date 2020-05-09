03b4fb2a03b29df81c3c7a9e19b841c8aae91129
bessonniy11 <bessonniy11@gmail.com>
2020-05-09 11:20:16 +0300

    added new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://www.whitechurch.co.uk/whitechurch-co-uk/_img/images/Artboard%202.png" alt=""/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://storage.googleapis.com/replit/images/1575910179162_be44cb648579f3e5693290578cf90ad6.jpeg" alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
